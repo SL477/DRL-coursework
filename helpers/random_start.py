9503d109a3188eeb5e37931d72b4f4edf5964267
Tom Fishwick <fishwickt@gmail.com>
2022-03-08 17:31:44 +0000

    Saved some of the data
    Sorted out early stopping and updated a picture

--- helpers/random_start.py
+++ helpers/random_start.py
@@ -4,7 +4,7 @@
 import numpy as np
 from .states_and_actions import states
 
-def random_start(given_map: np.array) -> int:
+def random_start(given_map: np.array, random_state=None) -> int:
     """Pick a random state to start in which is empty"""
     pos_states = states(given_map)
     
@@ -14,7 +14,13 @@ def random_start(given_map: np.array) -> int:
     value_of_s = 1
     num_states = len(pos_states)
     s = -1
-    while(value_of_s != 0) :
-        s = np.random.randint(0, num_states)
-        value_of_s = given_map[s // height, s % width]
+    if random_state is None:
+        while(value_of_s != 0) :
+            s = np.random.randint(0, num_states)
+            value_of_s = given_map[s // height, s % width]
+    else:
+        # this is to make sure the random numbers are random when this is working in parallel
+        while value_of_s != 0:
+            s = random_state.randint(0, num_states)
+            value_of_s = given_map[s // height, s % width]
     return s
